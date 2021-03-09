1b923e2011cbfcdb0651b0794fb6df0e5eb99db7
Barak Rosner <rosnerbb@gmail.com>
2021-03-09 09:28:23 -0500

    commit tree a900f5dad3ed7e37ac6ee050fdbbce238e961631

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,16 @@
 def clock_angle(time)
     # code your solution here
+    #converting time string to integers.
+    hours = time.split ":" [0].to_i
+    minutes = time.split ":" [1].to_i
+
+    #the hour hand moves a little every minute 
+    #360 degrees / 12 hours / 60 minutes = 0.5
+    hours_hand_degrees_per_minute = 0.5
+    hours_angle = 
+    minutes_angle = 
+
+    #Want return value to be a positive number
+    #needs to use .abs func. to return whole value.
+    angle = (hours_angle - minutes_angle).abs
 end
